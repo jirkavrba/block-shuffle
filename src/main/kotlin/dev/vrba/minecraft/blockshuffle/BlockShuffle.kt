0fe84b92186a9dfdfe8ef03c22c580078f08991c
Jiří Vrba <jiri@vrba.dev>
2021-07-26 16:34:59 +0200

    Remove shadow plugin as it is not required

--- src/main/kotlin/dev/vrba/minecraft/blockshuffle/BlockShuffle.kt
+++ src/main/kotlin/dev/vrba/minecraft/blockshuffle/BlockShuffle.kt
@@ -1,10 +1,12 @@
 package dev.vrba.minecraft.blockshuffle
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class BlockShuffle : JavaPlugin()
 {
     override fun onEnable()
     {
+        Bukkit.getLogger().info("Register BlockShuffle plugin")
     }
 }
